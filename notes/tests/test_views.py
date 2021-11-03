5c63db214ed53a4ce71f2a4fb3946ec5b747e9e8
Ahmed Saeed <mohameda7med64@gmail.com>
2021-11-03 18:32:27 +0200

    add tests for 'create' view

--- notes/tests/test_views.py
+++ notes/tests/test_views.py
@@ -9,6 +9,7 @@ class test_views(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.test_user = User.objects.create(username="testuser", password="12345678", email="test@user.mail")
 
     def test_notes_list_logged_in(self):
         self.client.force_login(User.objects.get_or_create(username='testuser')[0])
@@ -19,3 +20,37 @@ class test_views(TestCase):
     def test_notes_list_not_logged_in(self):
         response = self.client.get(reverse('index'))
         self.assertEquals(response.status_code, 302)
+
+    def test_create_notes_GET_logged_in(self):
+        self.client.force_login(User.objects.get_or_create(username='testuser')[0])
+        response = self.client.get(reverse('create'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('/notes/create.html')
+    
+    def test_create_notes_GET_not_logged_in(self):
+        response = self.client.get(reverse('create'))
+        self.assertRedirects(response, '/login?next=/create', status_code = 302,
+        target_status_code=200)
+
+    def test_create_notes_POST_logged_in(self):
+        self.client.force_login(User.objects.get_or_create(username='testuser')[0])
+        response = self.client.post(reverse('create'), {
+            'title': 'test title',
+            'body': 'test body'
+        })
+
+        self.assertRedirects(response, '/', status_code=302,
+         target_status_code=200, fetch_redirect_response=True)
+
+        self.assertEquals(Notes.objects.get(pk=1).title, 'test title')
+        self.assertEquals(Notes.objects.get(pk=1).body, 'test body')
+    
+    def test_create_notes_POST_logged_in(self):
+
+        response = self.client.post(reverse('create'), {
+            'title': 'test title',
+            'body': 'test body'
+        })
+
+        self.assertRedirects(response, '/login?next=/create', status_code=302,
+         target_status_code=200, fetch_redirect_response=True)
