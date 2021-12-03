c2c47668546a024b474c9e6d780814f2302bda88
Ahmed Saeed <mohameda7med64@gmail.com>
2021-12-03 04:16:49 +0200

    register models to admin app

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from notes.models import User, Notes, Favorites
 # Register your models here.
+admin.site.register(Notes)
+admin.site.register(User)
+admin.site.register(Favorites)
