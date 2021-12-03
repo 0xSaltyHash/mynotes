1c8bca0b420d94737155d349d91ac3ff1222c9f7
Ahmed Saeed <mohameda7med64@gmail.com>
2021-12-03 03:21:05 +0200

    Add url config for a path to list user favorite notes

--- notes/urls.py
+++ notes/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
      path("edit/<int:id>", views.edit, name="edit"),
      path("view/<int:id>", views.view_note, name="view_note"),
      path("change_password", views.change_password, name="change_password"),
-     path("favorite/<int:id>", views.favorite, name="favorite")
+     path("favorite/<int:id>", views.favorite, name="favorite"),
+     path("favorites", views.list_favorites, name="favorites")
  ]
