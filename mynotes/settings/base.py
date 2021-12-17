085a1ad842ce7525679329306d3f5fddf95600c5
Ahmed Saeed <mohameda7med64@gmail.com>
2021-12-17 10:12:21 +0200

    fix static files config

--- mynotes/settings/base.py
+++ mynotes/settings/base.py
@@ -144,3 +144,13 @@ MESSAGE_TAGS = {
  }
 
 
+
+PROJECT_ROOT   =   os.path.join(BASE_DIR)
+#print(PROJECT_ROOT)
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- mynotes/settings/heroku.py
+++ mynotes/settings/heroku.py
@@ -8,22 +8,14 @@ env = environ.Env(
 
 # False if not in os.environ
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = ['mynotesapp-1337.herokuapp.com']
+ALLOWED_HOSTS = ['mynotesapp-1337.herokuapp.com', "*"]
 SECRET_KEY = env('SECRET_KEY')
-DATABASES = {
+"""DATABASES = {
     # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
     'default': env.db(),
-}
+}"""
 
-PROJECT_ROOT   =   os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
